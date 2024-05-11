224bc1fbc918ad99998b71b0203e48c70e464dfb
octsnow <kawanof12@gmail.com>
2024-05-12 03:37:08 +0900

    update

--- UI.cpp
+++ UI.cpp
@@ -7,9 +7,74 @@ void Status::Update(OctGame* pOctGame, StatusData& data) {
     }
 }
 
+Menu::Menu() {
+    this->mX = 0;
+    this->mY = 0;
+}
+
 void Menu::Update(OctGame* pOctGame) {
     if(pOctGame == nullptr) return;
-    pOctGame->DrawBox(30, 30, 500, 500, 0x888888);
+
+    // key
+    if(pOctGame->IsDown('a')) {
+        this->Left();
+    }
+    if(pOctGame->IsDown('d')) {
+        this->Right();
+    }
+    if(pOctGame->IsDown('w')) {
+        this->Up();
+    }
+    if(pOctGame->IsDown('s')) {
+        this->Down();
+    }
+
+    // draw
+    pOctGame->DrawBox(MENU_LEFTTOP_X, MENU_LEFTTOP_Y, MENU_LEFTTOP_X + MENU_W, MENU_LEFTTOP_Y + MENU_H, 0x888888, true);
+    for(int x = 0; x < INVENTORY_W; x++) {
+        for(int y = 0; y < INVENTORY_H; y++) {
+            pOctGame->DrawBox(
+                    MENU_LEFTTOP_X + INVENTORY_LEFTTOP_X + (x * INVENTORY_GRIDSIZE),
+                    MENU_LEFTTOP_Y + INVENTORY_LEFTTOP_Y + (y * INVENTORY_GRIDSIZE),
+                    MENU_LEFTTOP_X + INVENTORY_LEFTTOP_X + ((x + 1) * INVENTORY_GRIDSIZE),
+                    MENU_LEFTTOP_Y + INVENTORY_LEFTTOP_Y + ((y + 1) * INVENTORY_GRIDSIZE),
+                    0x000000);
+        }
+    }
+    pOctGame->DrawBox(
+            MENU_LEFTTOP_X + INVENTORY_LEFTTOP_X + (this->mX * INVENTORY_GRIDSIZE),
+            MENU_LEFTTOP_Y + INVENTORY_LEFTTOP_Y + (this->mY * INVENTORY_GRIDSIZE),
+            MENU_LEFTTOP_X + INVENTORY_LEFTTOP_X + ((this->mX + 1) * INVENTORY_GRIDSIZE),
+            MENU_LEFTTOP_Y + INVENTORY_LEFTTOP_Y + ((this->mY + 1) * INVENTORY_GRIDSIZE),
+            0xFFFFFFFF);
+}
+
+void Menu::SetPosition(unsigned int x, unsigned int y) {
+    if(x >= INVENTORY_W) return;
+    if(y >= INVENTORY_H) return;
+
+    this->mX = x;
+    this->mY = y;
+}
+
+void Menu::Left() {
+    if(this->mX == 0) return;
+    this->mX--;
+}
+
+void Menu::Right() {
+    if(this->mX >= INVENTORY_W - 1) return;
+    this->mX++;
+}
+
+void Menu::Up() {
+    if(this->mY == 0) return;
+    this->mY--;
+}
+
+void Menu::Down() {
+    if(this->mY >= INVENTORY_H - 1) return;
+    this->mY++;
 }
 
 UI::UI() {
--- UI.hpp
+++ UI.hpp
@@ -1,4 +1,15 @@
 #include "OctGame/OctGame.hpp"
+#include "Item.hpp"
+
+#define INVENTORY_LEFTTOP_X 30
+#define INVENTORY_LEFTTOP_Y 30
+#define INVENTORY_W 10
+#define INVENTORY_H 5
+#define INVENTORY_GRIDSIZE 50
+#define MENU_LEFTTOP_X 30
+#define MENU_LEFTTOP_Y 30
+#define MENU_W (INVENTORY_W * INVENTORY_GRIDSIZE + INVENTORY_LEFTTOP_X * 2)
+#define MENU_H (INVENTORY_H * INVENTORY_GRIDSIZE + INVENTORY_LEFTTOP_Y * 2)
 
 struct StatusData {
     int coin;
@@ -12,7 +23,17 @@ public:
 
 class Menu {
 public:
+    Menu();
     void Update(OctGame* pOctGame);
+    void SetPosition(unsigned int x, unsigned int y);
+    void Left();
+    void Right();
+    void Up();
+    void Down();
+
+private:
+    HAT mInventory[INVENTORY_W * INVENTORY_H];
+    unsigned int mX, mY;
 };
 
 class UI {
